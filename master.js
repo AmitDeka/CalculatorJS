691bc6fd27cdb8aa6a02f9c46d728d4f1bbb0ea3
AmitDeka <amitdeka49@gmail.com>
2021-11-17 22:33:16 +0530

    Update Calculator with neumorphism design

--- master.js
+++ master.js
@@ -1,3 +1,22 @@
-function calcNumbers(result){
-    form.displayResult.value=form.displayResult.value+result;
-}
+const outputS = document.getElementById("outputResult");
+
+    function display(number){
+        outputS.value += number;
+    }
+
+    function calculate(){
+        try{
+            outputS.value = eval(outputS.value);
+        }
+        catch(err){
+            alert("Invalid argument !!!")
+        }
+    }
+
+    function Clear(){
+        outputS.value = "";
+    }
+
+    function Del(){
+        outputS.value = outputS.value.slice(0,-1);
+    }
